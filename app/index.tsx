96e8698dba1f8e6ba51abe5fc713d5f7df892fcd
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2026-02-26 14:43:22 +0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="items-center justify-center h-full w-full mx-auto">
+      <Text className="text-5xl text-primary font-bold">Welcome</Text>
+    </View>
+  );
+}
