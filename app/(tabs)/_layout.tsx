e92ee126930babf99d9f0350d40bec7ffc6d958e
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2026-02-27 17:56:02 +0600

    Add movie fetching functionality and search bar component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,7 +8,7 @@ const TabIcon = ({ focused, icon, title }: { focused: boolean; icon: any; title:
   if (focused) {
     return (
       <ImageBackground
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 items-center justify-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[100px] min-h-16 mt-4 items-center justify-center rounded-full overflow-hidden"
         source={images.highlight}
       >
         <Image source={icon} tintColor={"#151312"} className="size-5" />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,24 @@
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, View } from "react-native";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(
+    () =>
+      fetchMovies({
+        query: "",
+      }),
+    true,
+  );
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="w-full absolute z-0" />
@@ -15,6 +31,27 @@ export default function Index() {
         }}
       >
         <Image source={icons.logo} className="size-20 mt-20 mb-5 mx-auto self-center w-12 h-10" />
+        {moviesLoading ? (
+          <ActivityIndicator size={"large"} color={"#0000ff"} />
+        ) : moviesError ? (
+          <View>
+            <Text className="text-red-500">{moviesError.message}</Text>
+          </View>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar onPress={() => router.push("/search")} placeholder="Search movies, shows, actors..." />
+            <>
+              <Text className="text-white text-lg font-bold mt-5 mb-3">Trending Movies</Text>
+              <FlatList
+                keyExtractor={(item) => item.id.toString()}
+                data={movies}
+                renderItem={({ item }) => <Text className="text-white">{item.title}</Text>}
+                numColumns={3}
+                columnWrapperStyle={{ justifyContent: "flex-start", marginBottom: 10, gap: 20, paddingRight: 5 }}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
